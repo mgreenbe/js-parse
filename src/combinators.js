db6324c185120254f0d26b53a7c5d7d65914ed04
Matthew Greenberg <mgreenbe@Math-A20095.local>
2018-02-14 16:24:55 -0700

    seq with error handling

--- src/combinators.js
+++ src/combinators.js
@@ -1,54 +1,69 @@
 let {
-  succeed,
-  fail,
-  isSuccess,
   char,
-  oneOf,
-  noneOf,
-  regex,
-  mappedRegex,
-  letter,
-  letters,
   digit,
   digits,
-  spaces,
+  fail,
+  initPos,
+  isSuccess,
+  letter,
+  letters,
+  mappedRegex,
   maybeSpaces,
-  whitespace,
-  maybeWhitespace
-} = require('./parsers.js');
+  maybeWhitespace,
+  noneOf,
+  oneOf,
+  regex,
+  spaces,
+  succeed,
+  whitespace
+} = require("./parsers.js");
 
-let { doIncr } = require('./pos.js');
+let { doIncr } = require("./pos.js");
 
-function seq(parsers) {
+function __seq(parsers) {
   if (!Array.isArray(parsers)) {
-    throw new TypeError('You must pass an array to seq.');
+    throw new TypeError("You must pass an array to seq.");
   }
-  return function(str, pos) {
+  return function(str, endPrev) {
     if (parsers.length === 0) {
-      return succeed({ value: [], rest: str, pos });
+      return [];
     } else {
       let [p, ...ps] = parsers;
-      console.log(pos);
-      let pReply = p(str, pos);
+      let pReply = p(str, endPrev);
       if (isSuccess(pReply)) {
-        let psReply = seq(ps)(pReply.rest, pReply.pos);
-        if (isSuccess(psReply)) {
-          return succeed({
-            value: [pReply.value, ...psReply.value],
-            rest: psReply.rest,
-            pos: psReply.pos
-          });
-        } else {
-          return [pos, ...(Array.isArray(psReply) ? psReply : [psReply])];
-        }
+        let psReply = __seq(ps)(pReply.rest, pReply.end);
+        return [pReply, ...psReply];
       } else {
-        return pReply;
+        return [pReply];
       }
     }
-    // return { status: 'FAIL', parser: 'seq' };
+  };
+}
+
+function seq(parsers) {
+  return function(str, endPrev) {
+    let replies = __seq(parsers)(str, endPrev);
+    let n = replies.length;
+    if (n === 0) {
+      return succeed({ parser: seq, value: [], endPrev, end: endPrev });
+    }
+    if (isSuccess(replies[n - 1])) {
+      return succeed({
+        parser: "seq",
+        end: replies[n - 1].end,
+        endPrev,
+        replies
+      });
+    } else {
+      return fail({
+        parser: "seq",
+        endPrev,
+        replies
+      });
+    }
   };
 }
 
 let pos0 = { row: 1, col: 0, char: 0 };
-let p = seq([letters, digits, letters]);
-console.log(JSON.stringify(p('abc123###', pos0), null, 4));
+let p = seq([letters, whitespace, digits]);
+console.log(JSON.stringify(p("abc   d123", pos0), null, 4));
--- src/parsers.js
+++ src/parsers.js
@@ -1,101 +1,108 @@
-let { doIncr } = require('./pos.js');
+let { doIncr } = require("./pos.js");
 
 function succeed(obj) {
-  return Object.assign({ type: 'reply', status: 'success' }, obj || {});
+  return Object.assign({ type: "reply", status: "success" }, obj || {});
 }
 
 function fail(obj) {
-  return Object.assign({ type: 'reply', status: 'fail' }, obj || {});
+  return Object.assign({ type: "reply", status: "fail" }, obj || {});
 }
 
 function isSuccess(obj) {
-  return obj.status === 'success';
+  return obj.status === "success";
 }
 
 function isFailure(obj) {
   return !isSuccess(obj);
 }
 
+let initPos = { row: 1, col: 0, char: 0 };
+
 function char(c) {
-  return function(str, pos) {
+  return function(str, endPrev = initPos) {
     return c === str[0]
-      ? success({ value: c, rest: str.slice(1), pos: doIncr(c, pos) })
+      ? succeed({
+          parser: "char",
+          value: c,
+          rest: str.slice(1),
+          endPrev: endPrev,
+          end: doIncr(c, endPrev)
+        })
       : fail({
-          parser: 'char',
-          message: `Expected '${c}' at position (${pos.row}, ${pos.col +
-            1}). Found '${str[0]}' there.`
+          parser: "char",
+          expected: `${c}`,
+          found: `${str[0]}`,
+          endPrev
         });
   };
 }
 
 function oneOf(alphabet) {
-  if (typeof alphabet !== 'string') {
-    throw new TypeError('oneOf takes a string argument.');
+  if (typeof alphabet !== "string") {
+    throw new TypeError("oneOf takes a string argument.");
   }
-  return function(str, pos) {
+  return function(str, endPrev = initPos) {
     return alphabet.includes(str[0])
       ? succeed({
-          parser: 'oneOf',
+          parser: "oneOf",
           value: str[0],
           rest: str.slice(1),
-          pos: doIncr(str[0], pos)
+          endPrev: endPrev,
+          end: doIncr(str[0], endPrev)
         })
       : fail({
-          parser: 'oneOf',
-          message: `Expected a character of ${alphabet} at position (${
-            pos.row
-          }, ${pos.col + 1}). Found ${str[0]} there.`
+          parser: "oneOf",
+          expected: `a character of '${alphabet}'`,
+          found: `${str[0]}`,
+          endPrev
         });
   };
 }
 
 function noneOf(alphabet) {
-  return function(str, pos) {
-    let reply = oneOf(alphabet)(str, pos);
+  return function(str, endPrev = initPos) {
+    let reply = oneOf(alphabet)(str, endPrev);
     return isSuccess(reply)
       ? fail({
-          parser: 'noneOf',
-          expected: `Expected a character not in '${alphabet}' at position (${
-            pos.row
-          }, ${pos.col + 1}).`,
-          found: `Found '${reply.value[0]}'.`
+          parser: "noneOf",
+          expected: `a character not in '${alphabet}'`,
+          found: `${reply.value[0]}`,
+          endPrev
         })
       : success({
-          parser: 'noneOf',
+          parser: "noneOf",
           value: str[0],
           rest: str.slice(1),
-          pos: doIncr(str[0], pos)
+          endPrev: endPrev,
+          end: doIncr(str[0], endPrev)
         });
   };
 }
 
 function regex(re) {
-  return function(str, pos) {
-    let modifiedRE = new RegExp('^' + re.source, re.flags);
+  return function(str, endPrev = initPos) {
+    let modifiedRE = new RegExp("^" + re.source, re.flags);
     let match = modifiedRE.exec(str);
     return match !== null
       ? succeed({
-          parser: 'regex',
+          parser: "regex",
           value: match,
           rest: str.slice(match[0].length),
-          pos: doIncr(match[0], pos)
+          endPrev: endPrev,
+          end: doIncr(match[0], endPrev)
         })
       : fail({
-          parser: 'regex',
-          expected: `Expected to match /${modifiedRE.source}/${
-            modifiedRE.flags
-          } at position (${pos.row}, ${pos.col + 1}).`,
-          found: `Found '${str.slice(0, 10)}${
-            str.length <= 10 ? '' : '...'
-          }' there.`,
-          pos
+          parser: "regex",
+          expected: `/${modifiedRE.source}/${modifiedRE.flags}`,
+          found: `${str.slice(0, 10)}${str.length <= 10 ? "" : "..."}`,
+          endPrev
         });
   };
 }
 
 function mappedRegex(re, successReply, failReply) {
-  return function(str, pos) {
-    let reply = regex(re)(str, pos);
+  return function(str, endPrev = initPos) {
+    let reply = regex(re)(str, endPrev);
     return Object.assign(
       reply,
       isSuccess(reply) ? successReply(reply) : failReply(reply)
@@ -105,96 +112,96 @@ function mappedRegex(re, successReply, failReply) {
 
 let letter = mappedRegex(
   /[a-zA-Z]/,
-  ({ value }) => ({ parser: 'letter', value: value[0] }),
-  ({ pos }) => ({
-    parser: 'letter',
-    expected: `Expected a letter at position (${pos.row}, ${pos.col + 1}).`
+  ({ value }) => ({ parser: "letter", value: value[0] }),
+  ({ endPrev }) => ({
+    parser: "letter",
+    expected: `a letter`,
+    endPrev
   })
 );
 
 let letters = mappedRegex(
   /[a-zA-Z]+/,
-  ({ value }) => ({ parser: 'letters', value: value[0] }),
-  ({ pos }) => ({
-    parser: 'letters',
-    expected: `Expected a string of letters at position (${pos.row}, ${pos.col +
-      1}).`
+  ({ value }) => ({ parser: "letters", value: value[0] }),
+  ({ endPrev }) => ({
+    parser: "letters",
+    expected: `a string of letters`,
+    endPrev
   })
 );
 
 let digit = mappedRegex(
   /[0-9]/,
-  ({ value }) => ({ parser: 'digit', value: value[0] }),
-  ({ pos }) => ({
-    parser: 'letters',
-    expected: `Expected a digit at position (${pos.row}, ${pos.col + 1}).`
+  ({ value }) => ({ parser: "digit", value: value[0] }),
+  ({ endPrev }) => ({
+    parser: "letters",
+    expected: `a digit`,
+    endPrev
   })
 );
 
 let digits = mappedRegex(
   /[0-9]+/,
-  ({ value }) => ({ parser: 'digitd', value: value[0] }),
-  ({ pos }) => ({
-    parser: 'letters',
-    expected: `Expected a string of digits (length > 0) at position (${
-      pos.row
-    }, ${pos.col + 1}).`
+  ({ value }) => ({ parser: "digits", value: value[0] }),
+  ({ endPrev }) => ({
+    parser: "letters",
+    expected: `a string of digits (length > 0)`,
+    endPrev
   })
 );
 
 let spaces = mappedRegex(
   /[ ]+/,
-  ({ value }) => ({ parser: 'spaces', value: value[0] }),
-  ({ pos }) => ({
-    parser: 'spaces',
-    expected: `Expected a string of spaces (length > 0) at position (${
-      pos.row
-    }, ${pos.col + 1}).`
+  ({ value }) => ({ parser: "spaces", value: value[0] }),
+  ({ endPrev }) => ({
+    parser: "spaces",
+    expected: `a string of spaces (length > 0)`,
+    endPrev
   })
 );
 
 let maybeSpaces = mappedRegex(
   /[ ]*/,
-  ({ value }) => ({ parser: 'maybeSpaces', value: value[0] }),
+  ({ value }) => ({ parser: "maybeSpaces", value: value[0] }),
   () => {
-    throw new Error('maybeSpaces should never fail!');
+    throw new Error("maybeSpaces should never fail!");
   }
 );
 
 let whitespace = mappedRegex(
   /\s+/,
-  ({ value }) => ({ parser: 'whitespace', value: value[0] }),
-  ({ pos }) => ({
-    parser: 'spaces',
-    expected: `Expected whitespace (length > 0) at position (${
-      pos.row
-    }, ${pos.col + 1}).`
+  ({ value }) => ({ parser: "whitespace", value: value[0] }),
+  ({ endPrev }) => ({
+    parser: "spaces",
+    expected: `whitespace (length > 0)`,
+    endPrev
   })
 );
 
 let maybeWhitespace = mappedRegex(
   /\s*/,
-  ({ value }) => ({ parser: 'maybeWhitespace', value: value[0] }),
+  ({ value }) => ({ parser: "maybeWhitespace", value: value[0] }),
   () => {
-    throw new Error('maybeWhitepace should never fail!');
+    throw new Error("maybeWhitepace should never fail!");
   }
 );
 
 module.exports = {
-  succeed,
-  fail,
-  isSuccess,
   char,
-  oneOf,
-  noneOf,
-  regex,
-  mappedRegex,
-  letter,
-  letters,
   digit,
   digits,
-  spaces,
+  fail,
+  initPos,
+  isSuccess,
+  letter,
+  letters,
+  mappedRegex,
   maybeSpaces,
-  whitespace,
-  maybeWhitespace
+  maybeWhitespace,
+  noneOf,
+  oneOf,
+  regex,
+  spaces,
+  succeed,
+  whitespace
 };
