691495a68b784c0559f4a332f41e56431fac0c0a
Matthew Greenberg <mgreenbe@outlook.com>
2018-02-14 20:06:00 -0700

    refactored seq and alt, haven't cleaned up yet

--- src/combinators.js
+++ src/combinators.js
@@ -16,13 +16,13 @@ let {
   spaces,
   succeed,
   whitespace
-} = require("./parsers.js");
+} = require('./parsers.js');
 
-let { doIncr } = require("./pos.js");
+let { doIncr } = require('./pos.js');
 
 function __seq(parsers) {
   if (!Array.isArray(parsers)) {
-    throw new TypeError("You must pass an array to seq.");
+    throw new TypeError('You must pass an array to seq.');
   }
   return function(str, endPrev) {
     if (parsers.length === 0) {
@@ -45,25 +45,103 @@ function seq(parsers) {
     let replies = __seq(parsers)(str, endPrev);
     let n = replies.length;
     if (n === 0) {
-      return succeed({ parser: seq, value: [], endPrev, end: endPrev });
+      return succeed({ parser: 'seq', value: [], endPrev, end: endPrev });
     }
     if (isSuccess(replies[n - 1])) {
-      return succeed({
-        parser: "seq",
-        end: replies[n - 1].end,
-        endPrev,
-        replies
-      });
+      return Object.assign(
+        replies,
+        succeed({
+          parser: 'seq',
+          end: replies[n - 1].end,
+          endPrev
+        })
+      );
     } else {
-      return fail({
-        parser: "seq",
-        endPrev,
-        replies
-      });
+      return Object.assign(
+        replies,
+        fail({
+          parser: 'seq',
+          endPrev
+        })
+      );
+    }
+  };
+}
+
+// function __seq(parsers) {
+//   if (!Array.isArray(parsers)) {
+//     throw new TypeError('You must pass an array to seq.');
+//   }
+//   return function(str, endPrev) {
+//     if (parsers.length === 0) {
+//       return [];
+//     } else {
+//       let [p, ...ps] = parsers;
+//       let pReply = p(str, endPrev);
+//       if (isSuccess(pReply)) {
+//         let psReply = __seq(ps)(pReply.rest, pReply.end);
+//         return [pReply, ...psReply];
+//       } else {
+//         return [pReply];
+//       }
+//     }
+//   };
+// }
+
+function seqOrAlt(f, parser) {
+  return function(parsers) {
+    return function(str, endPrev) {
+      let replies = f(parsers)(str, endPrev);
+      let n = replies.length;
+      if (n === 0) {
+        return fail({ parser, value: [], endPrev, end: endPrev });
+      }
+      if (isSuccess(replies[n - 1])) {
+        return Object.assign(
+          replies,
+          succeed({
+            parser,
+            end: replies[n - 1].end,
+            endPrev
+          })
+        );
+      } else {
+        return Object.assign(
+          replies,
+          fail({
+            parser,
+            endPrev
+          })
+        );
+      }
+    };
+  };
+}
+
+let altt = seqOrAlt(__alt, 'alt');
+
+let seqq = seqOrAlt(__seq, 'seq');
+
+function __alt(parsers) {
+  if (!Array.isArray(parsers)) {
+    throw new TypeError('You must pass an array to seq.');
+  }
+  return function(str, endPrev) {
+    if (parsers.length === 0) {
+      return [];
+    } else {
+      let [p, ...ps] = parsers;
+      let pReply = p(str, endPrev);
+      if (isSuccess(pReply)) {
+        return [pReply];
+      } else {
+        let psReply = __alt(ps)(str, endPrev);
+        return [pReply, ...psReply];
+      }
     }
   };
 }
 
 let pos0 = { row: 1, col: 0, char: 0 };
-let p = seq([letters, whitespace, digits]);
-console.log(JSON.stringify(p("abc   d123", pos0), null, 4));
+let p = seqq([letters, whitespace, digits]);
+console.log(p('abc   123', pos0));
